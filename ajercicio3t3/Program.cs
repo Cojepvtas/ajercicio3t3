591c62af43a8989a0b47eb456849d635ec985933
itzaFc <itzaFc@DESKTOP-G0ERNLI>
2024-02-14 10:32:43 -0600

    Agregar archivos de proyecto.

---
+++ ajercicio3t3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ajercicio3t3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar el tipo de producto (ej. medicina, alimentos, otros):");
+            string tipoProducto = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Ingresar el precio del producto:");
+            double precio = Convert.ToDouble(Console.ReadLine());
+
+
+            double tasaImpuesto = 0.0;
+
+            if (tipoProducto == "Medicina")
+            {
+                tasaImpuesto = 0.0;
+            }
+            else if (tipoProducto == "Alimentos")
+            {
+                tasaImpuesto = 0.05;
+            }
+            else
+            {
+                tasaImpuesto = 0.1;
+            }
+
+            double impuesto = precio * tasaImpuesto;
+
+            Console.WriteLine("El impuesto a pagar es: $" + impuesto);
+        }
+    }
+
+}
+    
